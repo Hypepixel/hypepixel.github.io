721ef33229391d22711c7a7bf1f354d3a41862f0
JasperDre <jasperdre@gmail.com>
2020-04-13 00:26:10 +0200

    Add clipboard toast message

--- js/main.js
+++ js/main.js
@@ -7,3 +7,9 @@ clipboard.on('success', function(e) {
 clipboard.on('error', function(e) {
     console.log(e);
 });
+
+function launch_toast() {
+    var x = document.getElementById("toast")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
